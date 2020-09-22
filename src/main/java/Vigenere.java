7d445744bca7db9e729b53de39d7ba4eeba8833b
23eliu <23eliu@athenian.org>
2020-09-22 05:50:23 +0000

    started encrypt v

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -2,8 +2,42 @@ import java.util.Scanner;
 
 public class Vigenere {
     public static String encryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String efin = "";
+        int count = 0;
+        for (int i = 0; i < message.length(); i++) {
+            if (!(message.charAt(i) >= 65 && message.charAt(i) <= 90) && !(message.charAt(i) >= 97 && message.charAt(i) <= 122)) {
+                efin += message.charAt(i);
+            }
+            else if (message.charAt(i) >= 65 && message.charAt(i) <= 90) {
+                if (count == key.length()-1) {
+                    count = 0;
+                }
+                else {
+                    count++; 
+                }
+                int x = message.charAt(i) - 65;
+                x += (key.charAt(count) - 66);
+                if (x % 26 >= 0) {
+                    x = x % 26;
+                }
+                efin += (char) (x + 65);
+            }
+            else {
+                if (count == key.length()-1) {
+                    count = 0;
+                }
+                else {
+                    count++; 
+                }
+                int x = message.charAt(i) - 97;
+                x += (key.charAt(count) - 98);
+                if (x % 26 >= 0) {
+                    x = x % 26;
+                }
+                efin += (char) (x + 97);
+            }
+        }
+        return efin;
     }
 
     public static String decryptVigenere(String message, String key) {
